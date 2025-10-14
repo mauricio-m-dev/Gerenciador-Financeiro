dd886c6d963ca4ced76c36c508eaf343669c168c
Mauricio Martins <mauricio.m.silv4@gmail.com>
2025-10-14 12:33:48 -0300

    Estrutura HTML, estilos CSS e funcionalidade JavaScript para navegação

---
+++ View/VisaoGeral.php
@@ -0,0 +1,36 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../template/asset/css/VisaoGeral.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header class="header">
+        <div class="brand">NONYX</div>
+
+        <nav class="nav-links">
+            <a href="#" data-index="0" class="active">Início</a>
+            <a href="#" data-index="1">Sobre</a>
+            <a href="#" data-index="2">Serviços</a>
+            <a href="#" data-index="3">Portfólio</a>
+            <a href="#" data-index="4">Contato</a>
+        </nav>
+
+        <div class="profile-settings">
+            <i class="fas fa-cog settings-icon"></i>
+            <img src="https://via.placeholder.com/40" alt="Foto de Perfil" class="profile-pic" />
+        </div>
+    </header>
+
+    <script src="../template/asset/js/VisaoGeral.js"></script>
+</body>
+
+</html>
