ee7c9af7ebb47f292461c62fada5995506fc103c
Heitor-Braz <heitorbrazcm@gmail.com>
2025-10-21 11:51:07 -0300

    feat: adicionei os cards e ajustei o layout

---
+++ View/Analise.php
@@ -0,0 +1,92 @@
+<?php ?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>NOVYX - Análise</title>
+
+    <link rel="stylesheet" href="../template/asset/css/Analise.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    
+    <header>
+        <nav class="navbar">
+            
+            <div class="nav-left">
+                <div class="hamburger">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+                <a href="#" class="logo">NOVYX</a>
+            </div>
+
+            <ul class="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+
+            <div class="user-area">
+                <a href="#" class="settings-icon" aria-label="Configurações">
+                    <span class="material-symbols-outlined">
+                        <i class='bxr  bx-cog'  ></i> 
+                    </span>
+                </a>
+                <img src="" alt="Foto de Perfil" class="profile-pic">
+            </div>
+
+        </nav>
+    </header>
+
+     <main style="height: 200vh; padding: 100px 2rem; background-color: rgba(249, 249, 249, 1);">
+        <div class="title"><h1>Análise Financeira</h1></div>
+
+        <div class="container">
+            <div class="box">
+                <p class="mini">Patrimônio Total</p>
+                <h1>R$45.000</h1>
+                <p class="green"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M14 2.5a.5   .5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg>+12.5% vs mês anterior</p>
+            </div>
+            <div class="box">
+                <p class="mini">Patrimônio Total</p>
+                <h1>R$45.000</h1>
+                <p class="green"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M14 2.5a.5   .5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg>+12.5% vs mês anterior</p>
+            </div>
+            <div class="box">
+                <p class="mini">Patrimônio Total</p>
+                <h1>R$45.000</h1>
+                <p class="green"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M14 2.5a.5   .5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg>+12.5% vs mês anterior</p>
+            </div>
+            <div class="box">
+                <p class="mini">Patrimônio Total</p>
+                <h1>R$45.000</h1>
+                <p class="green"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M14 2.5a.5   .5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+</svg>+12.5% vs mês anterior</p>
+            </div>
+        </div>
+      
+    </main>
+    <script src="../template/asset/js/Analise.js"></script>
+</body>
+
+</html>
