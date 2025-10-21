ef169123395aa108ed37f3a415b5ed388c347f32
dev-luisgustavo <luisgustavofe2105@gmail.com>
2025-10-21 12:30:47 -0300

    feat: Cards adcionados

--- View/Investimento.php
+++ View/Investimento.php
@@ -4,33 +4,91 @@
 <html lang="en">
 
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../template/asset/css/VisaoGeral.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Visao Geral - NOVYX</title>
+
+    <link rel="stylesheet" href="../template/asset/css/Investimento.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
+
 </head>
 
+
 <body>
-    <header class="header">
-        <div class="brand">NONYX</div>
+   <header>
+        <nav class="navbar">
+            <div class="nav-left">
 
-        <nav class="nav-links">
-            <a href="#" data-index="0" class="active">Início</a>
-            <a href="#" data-index="1">Sobre</a>
-            <a href="#" data-index="2">Serviços</a>
-            <a href="#" data-index="3">Portfólio</a>
-            <a href="#" data-index="4">Contato</a>
+                <div class="hamburger">
+
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+
+                </div>
+                <a href="#" class="logo">NOVYX</a>
+            </div>
+
+            <ul class="nav-links">
+
+                <li><a href="#">Visão Geral</a></li>
+                <li><a href="#">Investimentos</a></li>
+                <li><a href="#">Análise</a></li>
+                <li><a href="#">Metas</a></li>
+                <li><a href="#">Cartões</a></li>
+
+            </ul>
+
+            <div class="user-area">
+                <a href="#" class="settings-icon" aria-label="Configurações">
+                    <span class="material-symbols-outlined">
+                        <i class='bxr  bx-cog'></i>
+                    </span>
+                </a>
+                <img src="" alt="Foto de Perfil" class="profile-pic">
+            </div>
         </nav>
-
-        <div class="profile-settings">
-            <i class="fas fa-cog settings-icon"></i>
-            <img src="https://via.placeholder.com/40" alt="Foto de Perfil" class="profile-pic" />
-        </div>
     </header>
 
-    <script src="../template/asset/js/VisaoGeral.js"></script>
+    <main style="height: 200vh; padding: 100px 2rem; background-color: #f9f9f9;">
+        
+
+        <div class="container">
+
+            <div class="titulo">
+            
+            <h1>Investimentos</h1>
+
+            <button class="adicionar"> <i class='bxr  bx-plus'  ></i> Adicionar Investimento</button>
+        </div>
+
+            <div class="geral-container">
+                <div class="geral-card-renda">
+                    <p>Renda</p>
+                    <h2>R$ 147.000</h2>
+                </div>
+                <div class="geral-card-despesas">
+                    <p>Despesas</p>
+                    <h2>R$ 59.700</h2>
+                </div>
+                <div class="geral-card-metas">
+                    <p>Metas</p>
+                    <h2>R$ 45.909</h2>
+                </div>
+            </div>
+        </div>
+
+
+    </main>
+
+    <script src="../template/asset/js/Investimento.js"></script>
 </body>
 
 </html>
