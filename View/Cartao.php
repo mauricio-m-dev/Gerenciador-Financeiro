f6e3bb4990bf9cf977f9839bd456f380b71d1a3b
Henriquerockstrar <macedohenrique322@gmail.com>
2025-10-21 12:09:05 -0300

    feat: Atualizações

---
+++ View/Cartao.php
@@ -0,0 +1,68 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../template/asset/css/Cartao.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    </head>
+
+<body>
+    <header class="header">
+        <div class="brand">NONYX</div>
+     
+        <nav class="nav-links">
+            <a href="#" data-index="0" class="active">Visão Geral</a>
+            <a href="#" data-index="1">Investimentos</a>
+            <a href="#" data-index="2">Análise</a>
+            <a href="#" data-index="3">Metas</a>
+            <a href="#" data-index="4">Cartões</a>
+        </nav>
+
+        <div class="profile-settings">
+            <i class="fas fa-cog settings-icon"></i>
+            <img src="" alt="Foto de Perfil" class="profile-pic" />
+        </div>
+    </header>
+   <main class="main-content1">
+        <h2>Cartões</h2>
+        <div class="add-cartao">
+        <i class='bx bx-plus'></i>
+            <span>Adicionar Cartão</span>
+        </div>
+            <div class="Minha-carteira">
+        <h2>Carteiras</h2>
+        <h5>Selecione uma carteira para ver os detalhes</h5>
+        </div>
+        <div class="main-content2">
+            <div class="Renda">
+                <h4>Renda</h4>
+
+            </div>
+            <div class="Despesas">
+                <h4>Despesas</h4>
+
+            </div>
+            <div class="Metas">
+                <h4>Metas</h4>
+
+
+            </div>
+        </div>
+        
+            
+
+    <script src="../template/asset/js/Cartao.js"></script>
+</body>
+
+</html>
